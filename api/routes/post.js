af8e713f30076346cddf353f62bf7482aab65d98
josymarss <sylvajosymar@gmail.com>
2020-10-17 00:46:06 +0100

    Some security

--- api/routes/post.js
+++ api/routes/post.js
@@ -1,10 +1,14 @@
+const bcrypt = require('bcrypt')
 const express = require('express')
 const transfer = express.Router()
+
+
 
 //Only routes for transfer money 
 
 transfer.post('/post/transfer', (res, req) => {
      //Make an transfer 
+     //generate a secure encriptation using bcrypt
  })
  
 
